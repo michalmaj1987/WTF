2ae511e59fc273558cf5370982181cf80aa78b42
Michal Maj <michalmaj1987.com>
2020-05-18 14:16:15 +0200

    praca domwa z tygodnia 5 bez wizytówki

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ const age = 32;
 console.log(name);
 console.log(age);
 
-console.log(`Nazywam się ${name} i mam ${age} lata. `)
+const greet = (age,name) =>{
+console.log(`Witaj Nazywam się ${name} i mam ${age} lata. `);
+}
+
+greet(32,'Michał');
+
+const header = document.querySelector('.main__header--js');
+header.innerHTML = `Witaj Nazywam się ${name} i mam ${age} lata.`;
+const emptyParagraph = document.querySelector('.about__header__paragraph--js');
+emptyParagraph.innerHTML = `Jeszcze nie uzupełniłem treści o CSS ale zrobiłem JS!`;
